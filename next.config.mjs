793b278510ad44ce6cf391306532770b71a52837
zysbau <zysbau@gmail.com>
2025-06-05 12:49:54 +0300

    deploy updated static site again

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export', 
+  output: 'export',
+  basePath: '/kebabas',       // 👈 your repo name
+  assetPrefix: '/kebabas',    // 👈 your repo name
 }
 
-export default nextConfig
-
+export default nextConfig;
